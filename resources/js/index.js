8b6645e801035dea22821518903edab61a783c38
Steve <steve.gitprofile@outlook.com>
2024-02-21 22:22:32 +0200

    added event listener on body

---
+++ resources/js/index.js
@@ -0,0 +1,14 @@
+function displayKeyInfo(key, code, keyCode) {
+  const info = [key, code, keyCode];
+  const keyInfoArr = document.querySelectorAll('.info');
+  for (let i in keyInfoArr) {
+    keyInfoArr[i].innerHTML = `<p>${info[i]}</p>`;
+  }
+}
+
+document.body.addEventListener('keypress', (e) => {
+  const keyCode = e.keyCode;
+  const key = e.key;
+  const code = e.code;
+  displayKeyInfo(key, code, keyCode);
+});
